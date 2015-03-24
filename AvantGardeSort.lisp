e8b50e56ca2cca7a4b0316fbb609514e17b918c8
mmanguno <mmanguno3@gatech.edu>
2015-03-24 11:25:27 -0400

    Adds the Common Lisp implementation of AvantGardeSort.

---
+++ AvantGardeSort.lisp
@@ -0,0 +1,7 @@
+(defpackage #:avant-garde-sort
+  (:use #:common-lisp)
+  (:export #:avant-garde-sort))
+
+(defun avant-garde-sort (&rest args)
+  "Defines 'sorted' to be the sequence taken in."
+   args)
