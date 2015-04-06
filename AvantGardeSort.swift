514a456201d0e70ac1bbbfa080a92db1bf9c6cd1
Jonathan Jemson <jonathanjemson@gatech.edu>
2015-04-06 10:21:05 -0400

    Add Swift implementation

---
+++ AvantGardeSort.swift
@@ -0,0 +1,13 @@
+//
+//  AvantGardeSort.swift
+//  
+//
+//  Created by Jonathan Jemson on 4/6/15.
+//
+//
+
+public class AvantGardeSort {
+    public class func avantgardeSort<T> (elements: [T]) -> [T] {
+        return elements;
+    }
+}
