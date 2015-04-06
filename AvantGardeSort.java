4b3aa0a84923ef1e982748a9c3e52914364e7461
mmanguno <mmanguno3@gatech.edu>
2015-04-06 08:31:32 -0400

    Adds the Java implementation of Avant-Garde sort.

---
+++ AvantGardeSort.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+/**
+ * Sorts in O(1) time by defining 'sorted' to be the ordering of the inputted
+ * list.
+ *
+ * @author Mitchell Manguno
+ * @version 1.0
+ * @since 2015 April 6
+ */
+public class AvantGardeSort {
+
+    /**
+     * Sorts the list using thd Avant-Garde sort methodology.
+     *
+     * @param list the list to be sorted
+     * @return the Avant-Garde'd list
+     */
+    public <T> List<T> sort(List<T> list) {
+        return list;
+    }
+}
